4beb3c4f346495f13e1d09151e2fa0231e0e558a
omar-mfarrag <omar.mustafa.ff@gmail.com>
2024-03-17 04:20:36 +0200

    end of theme change script

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,16 +1,48 @@
-let theme = 'light';
-// for switch wallpaper between mobile and pc
-window.addEventListener('load',() => {
-    if (document.body.offsetWidth < 800) {
-        document.getElementById('background').src =`./images/bg-mobile-${theme}.jpg`;
-    }else{
-        document.getElementById('background').src =`./images/bg-desktop-${theme}.jpg`;
+let theme; //declar a theme variable
+let themeIcon = document.getElementById('theme-icon'); // to get Theme Icon refrunce
+
+if (localStorage.getItem('theme')) { // if we have a theme in local storage === true
+    theme = localStorage.getItem('theme'); //giv theme value of theme in local storage
+    themeChange(); // if we have a local storage with theme key run themeChange function
+} else { // if we don't have theme in local storage
+    theme = 'light'; //giv theme the default value ==> "light"
+}
+
+// for change background with resize
+window.addEventListener('resize', () => {
+    backgroundChange();
+});
+
+// for add event on theme icon
+themeIcon.addEventListener('click', () => { // to add click event on themeIcon
+    if (theme === 'light') {
+        localStorage.setItem('theme', 'dark'); //  set a value for theme key
+        theme = 'dark';
+    } else {
+        localStorage.setItem('theme', 'light'); // set a value for theme key
+        theme = 'light';
     }
+    themeChange(); // call back themeChange funciton
 })
-window.addEventListener('resize',() => {
-    if (document.body.offsetWidth < 800) {
-        document.getElementById('background').src =`./images/bg-mobile-${theme}.jpg`;
-    }else{
-        document.getElementById('background').src =`./images/bg-desktop-${theme}.jpg`;
+
+// this function for theme changes
+function themeChange() {
+    backgroundChange();
+    if (theme === 'dark') { // if theme value = "dark"
+        document.body.classList.add('dark');
+        themeIcon.className = 'fa-solid fa-sun'; // change theme icon to sun
+    } else { // if theme value = 'light'
+        document.body.classList.remove('dark');
+        themeIcon.className = 'fa-solid fa-moon'; //change theme icon to moon
     }
-})
+}
+
+// this function for background change with size and theme
+function backgroundChange() {
+    if (document.body.offsetWidth < 470) { // add mobile back ground if body size is smaller than 470px
+        document.getElementById('background').src = `./images/bg-mobile-${theme}.jpg`;
+    } else { // add mobile back ground if body size is bigger than or equal 470px
+        document.getElementById('background').src = `./images/bg-desktop-${theme}.jpg`;
+    }
+}
+// END OF THEME CHANGE SCRIPT /////////////////////////////////////////////////////////////////////////////////////////////////
